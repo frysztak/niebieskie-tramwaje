5aacdfb823c2003807341e6bc3890070883bb7b4
Sebastian Frysztak <sebfry@gmail.com>
2018-05-16 18:33:50 +0200

    Revert "introduce common base activity used by MainActivity and StopActivity"
    This reverts commit b84af27bb536308c1364358901aafe4d684ef0f4.

--- app/src/main/java/com/orpington/software/rozkladmpk/MainActivity.kt
+++ app/src/main/java/com/orpington/software/rozkladmpk/MainActivity.kt
@@ -1,32 +1,66 @@
 package com.orpington.software.rozkladmpk
 
+import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import net.grandcentrix.thirtyinch.TiActivity
+import android.content.Intent
 
 
-class MainActivity : MainActivityTemplate()
-{
-    override val itemClickListener: RecyclerViewClickListener? = object: RecyclerViewClickListener {
-        override fun onClick(view: View, position: Int) {
-            transportLinesPresenter.onItemClicked(position)
-            Toast.makeText(applicationContext, "Position " + position, Toast.LENGTH_SHORT).show()
-        }
+
+
+class MainActivity : TiActivity<MainPresenter, NavigatingView>(), NavigatingView {
+
+    override fun providePresenter(): MainPresenter {
+        return MainPresenter()
     }
 
-    override val searchViewTextListener: SearchView.OnQueryTextListener? = object: SearchView.OnQueryTextListener {
-        override fun onQueryTextChange(newText: String?): Boolean {
-            var result = transportLinesPresenter.onQueryTextChange(newText)
-            transportLinesAdapter.notifyDataSetChanged()
-            return result
-        }
+    private lateinit var transportLinesAdapter: TransportLineListAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        override fun onQueryTextSubmit(newText: String?): Boolean {
-            return true
+        var interactor = TransportLinesInteractorImpl(baseContext)
+        var transportLinesPresenter = TransportLinesPresenter(interactor, this)
+        val listener = object: RecyclerViewClickListener {
+            override fun onClick(view: View, position: Int) {
+                transportLinesPresenter.onItemClicked(position)
+                Toast.makeText(applicationContext, "Position " + position, Toast.LENGTH_SHORT).show()
+            }
         }
+        transportLinesAdapter = TransportLineListAdapter(this, transportLinesPresenter, listener)
+        searchView?.queryHint = "Przystanek..."
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                var result = transportLinesPresenter.onQueryTextChange(newText)
+                transportLinesAdapter.notifyDataSetChanged()
+                return result
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+        var layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = transportLinesAdapter
+        val dividerItemDecoration = DividerItemDecoration(
+                applicationContext,
+                layoutManager.orientation
+        )
+        recyclerView.addItemDecoration(dividerItemDecoration)
     }
 
-    override fun loadData() {
-
+    override fun navigateToStopActivity(stopName: String) {
+        val i = Intent(baseContext, StopActivity::class.java)
+        i.putExtra("stopName", stopName)
+        startActivity(i)
     }
+
+
 }
--- app/src/main/java/com/orpington/software/rozkladmpk/StopActivity.kt
+++ app/src/main/java/com/orpington/software/rozkladmpk/StopActivity.kt
@@ -1,14 +1,47 @@
 package com.orpington.software.rozkladmpk
 
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.view.MenuItem
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_station.*
 
-class StopActivity :  MainActivityTemplate() {
+
+class StopActivity : AppCompatActivity(), NavigatingView {
+    override fun navigateToStopActivity(stationName: String) {
+        // remove me
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        searchView?.visibility = View.GONE
+        setContentView(R.layout.activity_station)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var stopName = intent.getStringExtra("stopName")
+
+        var interactor = TransportLinesInteractorImpl(baseContext)
+        var transportLinesPresenter = TransportLinesPresenter(interactor, this)
+        val listener = object: RecyclerViewClickListener {
+            override fun onClick(view: View, position: Int) {
+                transportLinesPresenter.onItemClicked(position)
+            }
+        }
+        var transportLinesAdapter = TransportLineListAdapter(this, transportLinesPresenter, listener)
+
+        var layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = transportLinesAdapter
+        val dividerItemDecoration = DividerItemDecoration(
+                applicationContext,
+                layoutManager.orientation
+        )
+        recyclerView.addItemDecoration(dividerItemDecoration)
+
+        title = stopName
+        transportLinesPresenter.loadRoutesForStop(stopName)
+        transportLinesAdapter.notifyDataSetChanged()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -17,15 +50,8 @@ class StopActivity :  MainActivityTemplate() {
                 onBackPressed()
                 return true
             }
-            else -> return false //super.onOptionsItemSelected(item)
+            else -> return super.onOptionsItemSelected(item)
         }
-    }
 
-    override fun loadData() {
-        val stopName = intent.getStringExtra("stopName")
-        title = stopName
-        transportLinesPresenter.loadRoutesForStop(stopName)
-        transportLinesAdapter.notifyDataSetChanged()
     }
 }
-
