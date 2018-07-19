99fffeaf9c0a47b395f5b2862e4f59e2fe6f1782
Sebastian Frysztak <sebfry@gmail.com>
2018-07-19 17:25:56 +0200

    Timetable: UI refinements

--- app/src/main/java/com/orpington/software/rozkladmpk/timetable/TimetableActivity.kt
+++ app/src/main/java/com/orpington/software/rozkladmpk/timetable/TimetableActivity.kt
@@ -3,6 +3,7 @@ package com.orpington.software.rozkladmpk.timetable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.MenuItem
 import com.orpington.software.rozkladmpk.Injection
 import com.orpington.software.rozkladmpk.R
 import com.orpington.software.rozkladmpk.data.model.TimeTable
@@ -17,18 +18,26 @@ class TimetableActivity : AppCompatActivity(), TimetableContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timetable)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         presenter = TimetablePresenter(Injection.provideDataSource(cacheDir), this)
         adapter = TimetableRecyclerViewAdapter(this, presenter)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
 
+        val routeID = intent.getStringExtra("routeID")
+        val atStop = intent.getStringExtra("atStop")
+        val fromStop = intent.getStringExtra("fromStop")
+        val toStop = intent.getStringExtra("toStop")
+
         presenter.loadTimeTable(
-            intent.getStringExtra("routeID"),
-            intent.getStringExtra("atStop"),
-            intent.getStringExtra("fromStop"),
-            intent.getStringExtra("toStop")
+            routeID,
+            atStop,
+            fromStop,
+            toStop
         )
+
+        title = "$fromStop - $toStop"
     }
 
     override fun hideProgressBar() {
@@ -47,4 +56,15 @@ class TimetableActivity : AppCompatActivity(), TimetableContract.View {
         adapter.setTimeTable(timeTable)
     }
 
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+    }
 }
