0afc35e92262cf0000cddb72cd2fd47f242f0c33
Sebastian Frysztak <sebfry@gmail.com>
2018-07-22 11:06:18 +0200

    Timetable: use extensions methods instead of nasty, java-like anonymous objects

--- app/src/main/java/com/orpington/software/rozkladmpk/timetable/TimetableActivity.kt
+++ app/src/main/java/com/orpington/software/rozkladmpk/timetable/TimetableActivity.kt
@@ -3,14 +3,13 @@ package com.orpington.software.rozkladmpk.timetable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.MenuItem
-import android.view.ViewTreeObserver
 import android.widget.TextView
 import com.orpington.software.rozkladmpk.Injection
 import com.orpington.software.rozkladmpk.R
-import com.orpington.software.rozkladmpk.utils.RecyclerViewReadyCallback
+import com.orpington.software.rozkladmpk.utils.afterMeasured
 import com.orpington.software.rozkladmpk.utils.forceRippleAnimation
+import com.orpington.software.rozkladmpk.utils.whenScrollStateIdle
 import kotlinx.android.synthetic.main.activity_timetable.*
 import mu.KotlinLogging
 
@@ -63,38 +62,23 @@ class TimetableActivity : AppCompatActivity(), TimetableContract.View {
     override fun showTimeTable(items: List<TimetablePresenter.ViewItem>, timeToScrollInto: TimeIndices) {
         adapter.setItems(items)
 
-        val recyclerViewScrollListener = object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                logger.debug { "scroll state changed. newState: $newState" }
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    val vh = recyclerView?.findViewHolderForAdapterPosition(timeToScrollInto.hourIdx)
+        recyclerView.afterMeasured {
+            logger.debug { "RecyclerView ready" }
+            if (timeToScrollInto.hourIdx >= 0 && adapter.itemCount > timeToScrollInto.hourIdx) {
+
+                this.whenScrollStateIdle {
+                    val vh = this.findViewHolderForAdapterPosition(timeToScrollInto.hourIdx)
                     if (vh is TimetableRecyclerViewAdapter.RowViewHolder && vh.linearLayout.childCount > timeToScrollInto.minuteIdx) {
                         val textView = vh.linearLayout.getChildAt(timeToScrollInto.minuteIdx) as TextView
                         logger.debug { "found textview. forcing ripple animation." }
                         textView.forceRippleAnimation()
                     }
-                    recyclerView?.removeOnScrollListener(this)
                 }
-            }
-        }
 
-        val recyclerViewReadyCallback = object : RecyclerViewReadyCallback {
-            override fun onLayoutReady() {
-                logger.debug { "RecyclerView ready" }
-                if (timeToScrollInto.hourIdx >= 0 && adapter.itemCount > timeToScrollInto.hourIdx) {
-                    recyclerView.addOnScrollListener(recyclerViewScrollListener)
-                    logger.debug { "smooth scrolling to index $timeToScrollInto.hourIdx" }
-                    recyclerView.smoothScrollToPosition(timeToScrollInto.hourIdx)
-                }
+                logger.debug { "smooth scrolling to index $timeToScrollInto.hourIdx" }
+                this.smoothScrollToPosition(timeToScrollInto.hourIdx)
             }
         }
-        recyclerView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                recyclerViewReadyCallback.onLayoutReady()
-                recyclerView.viewTreeObserver.removeOnGlobalLayoutListener(this)
-            }
-        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
--- app/src/main/java/com/orpington/software/rozkladmpk/utils/UIUtils.kt
+++ app/src/main/java/com/orpington/software/rozkladmpk/utils/UIUtils.kt
@@ -3,10 +3,12 @@ package com.orpington.software.rozkladmpk.utils
 import android.graphics.drawable.RippleDrawable
 import android.os.Build
 import android.os.Handler
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewTreeObserver
 
 // https://stackoverflow.com/a/32301440
-fun View.forceRippleAnimation() {
+inline fun View.forceRippleAnimation() {
     if (Build.VERSION.SDK_INT >= 21 && background is RippleDrawable) {
         val rippleDrawable = background as RippleDrawable
 
@@ -17,3 +19,25 @@ fun View.forceRippleAnimation() {
     }
 }
 
+// https://antonioleiva.com/kotlin-ongloballayoutlistener/
+inline fun <T: View> T.afterMeasured(crossinline f: T.() -> Unit) {
+    viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+        override fun onGlobalLayout() {
+            if (measuredWidth > 0 && measuredHeight > 0) {
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+                f()
+            }
+        }
+    })
+}
+
+inline fun RecyclerView.whenScrollStateIdle(crossinline f: RecyclerView.() -> Unit) {
+    addOnScrollListener(object: RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+            if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                removeOnScrollListener(this)
+                f()
+            }
+        }
+    })
+}
