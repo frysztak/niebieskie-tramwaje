d7e2b222239aa7a5d3624faa135756c3380162a6
Sebastian Frysztak <sebfry@gmail.com>
2018-07-31 22:19:12 +0200

    add LeakCanary

--- app/src/main/java/com/orpington/software/rozkladmpk/RozkladMPK.kt
+++ app/src/main/java/com/orpington/software/rozkladmpk/RozkladMPK.kt
@@ -1,11 +1,16 @@
 package com.orpington.software.rozkladmpk
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import org.slf4j.impl.HandroidLoggerAdapter
 
-class RozkladMPK: Application() {
+class RozkladMPK : Application() {
     override fun onCreate() {
         super.onCreate()
         HandroidLoggerAdapter.DEBUG = BuildConfig.DEBUG
+
+        if (BuildConfig.DEBUG) {
+            LeakCanary.install(this)
+        }
     }
 }
