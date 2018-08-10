76425d564d7687b14c12454259a5da5d3e5d08a6
Sebastian Frysztak <sebfry@gmail.com>
2018-08-10 12:21:24 +0200

    StopsAndRoute: more intelligent searching

--- app/src/main/java/com/orpington/software/rozkladmpk/stopsAndRoutes/StopsAndRoutesHelper.kt
+++ app/src/main/java/com/orpington/software/rozkladmpk/stopsAndRoutes/StopsAndRoutesHelper.kt
@@ -1,6 +1,8 @@
 package com.orpington.software.rozkladmpk.stopsAndRoutes
 
 import com.orpington.software.rozkladmpk.data.model.StopsAndRoutes
+import me.xdrop.fuzzywuzzy.FuzzySearch
+
 
 sealed class StopOrRoute
 
@@ -48,13 +50,72 @@ class StopsAndRoutesHelper {
         items: List<StopOrRoute>,
         query: String
     ): List<StopOrRoute> {
-        return items.filter { item ->
-            when (item) {
-                is Stop -> item.stopName.startsWith(query, true)
-                is Route -> item.routeID.startsWith(query, true)
-            }
 
+        if (query.isEmpty()) {
+            return items
         }
+
+        val threshold = 70
+        val normalisedQuery = query.stripAccents()
+
+        // handle cases like 'A', 'C' etc which correspond to express bus lines
+        if (normalisedQuery.length < 2) {
+            val routes = items.filterIsInstance(Route::class.java)
+                .filter { route ->
+                    route.routeID.startsWith(normalisedQuery, true)
+                }
+            if (routes.isNotEmpty()) return routes
+        }
+
+        // handle every other case
+        return items.map { item ->
+            val str =
+                when (item) {
+                    is Stop -> item.stopName
+                    is Route -> item.routeID
+                }.stripAccents()
+
+            val score =
+                if (str.contains(normalisedQuery, true))
+                    100
+                else
+                    FuzzySearch.ratio(normalisedQuery, str)
+
+            Pair(item, score)
+        }.filter { pair ->
+            pair.second > threshold
+        }.sortedByDescending { pair ->
+            pair.second //score
+        }.map { pair ->
+            pair.first
+        }
+    }
+
+    // https://stackoverflow.com/a/10831704
+    private fun String.stripAccents(): String {
+        val tab00c0 = "AAAAAAACEEEEIIII" +
+            "DNOOOOO\u00d7\u00d8UUUUYI\u00df" +
+            "aaaaaaaceeeeiiii" +
+            "\u00f0nooooo\u00f7\u00f8uuuuy\u00fey" +
+            "AaAaAaCcCcCcCcDd" +
+            "DdEeEeEeEeEeGgGg" +
+            "GgGgHhHhIiIiIiIi" +
+            "IiJjJjKkkLlLlLlL" +
+            "lLlNnNnNnnNnOoOo" +
+            "OoOoRrRrRrSsSsSs" +
+            "SsTtTtTtUuUuUuUu" +
+            "UuUuWwYyYZzZzZzF"
+
+        val vysl = CharArray(this.length)
+        var one: Char
+        for (i in 0 until this.length) {
+            one = this[i]
+            if (one in '\u00c0'..'\u017f') {
+                one = tab00c0[one.toInt() - '\u00c0'.toInt()]
+            }
+            vysl[i] = one
+        }
+        return String(vysl)
     }
 
 }
