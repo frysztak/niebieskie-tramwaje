13b1ae54c9d6c69ef7d44e58d20d92e0b19a138b
Sebastian Frysztak <sebfry@gmail.com>
2018-08-12 12:17:33 +0200

    restore TimetableHelperTest

--- app/src/test/java/com/orpington/software/rozkladmpk/TimetableHelperTest.kt
+++ app/src/test/java/com/orpington/software/rozkladmpk/TimetableHelperTest.kt
@@ -3,6 +3,8 @@ package com.orpington.software.rozkladmpk
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.orpington.software.rozkladmpk.routeDetails.TimetableHelper
+import com.orpington.software.rozkladmpk.routeDetails.TimetableViewHelper
+import com.orpington.software.rozkladmpk.routeDetails.TimetableViewHelper.*
 import junit.framework.Assert
 import org.junit.Test
 import org.junit.experimental.runners.Enclosed
@@ -13,7 +15,6 @@ import java.util.*
 @RunWith(Enclosed::class)
 class TimetableHelperTest {
 
-    /*
     @RunWith(Parameterized::class)
     class GetCurrentDayTypeTest(
         private val currentDay: Int,
@@ -31,7 +32,7 @@ class TimetableHelperTest {
                     arrayOf(Calendar.THURSDAY, DayType.Weekday),
                     arrayOf(Calendar.FRIDAY, DayType.Weekday),
                     arrayOf(Calendar.SATURDAY, DayType.Saturday),
-                    arrayOf(Calendar.SUNDAY, DayType.Sunday)
+                    arrayOf(Calendar.SUNDAY, TimetableViewHelper.DayType.Sunday)
                 )
             }
         }
@@ -53,7 +54,7 @@ class TimetableHelperTest {
         private val currentDay: Int,
         private val currentHour: Int,
         private val currentMinute: Int,
-        private val items: List<ViewItem>,
+        private val items: List<TimetableViewHelper.ViewItem>,
         private val expectedRowIndex: Int,
         private val expectedColumnIndex: Int
     ) {
@@ -66,41 +67,41 @@ class TimetableHelperTest {
                     // 0: Typical situation
                     arrayOf(Calendar.MONDAY, 12, 6, listOf(
                         HeaderItem(DayType.Weekday, ""),
-                        RowItem(arrayListOf("10", "07")),
-                        RowItem(arrayListOf("11", "07")),
-                        RowItem(arrayListOf("12", "07"))
+                        RowItem(DayType.Weekday, arrayListOf("10", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("11", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("12", "07"))
                     ), 3, 0),
 
                     // 1: Typical situation
                     arrayOf(Calendar.MONDAY, 12, 50, listOf(
                         HeaderItem(DayType.Weekday, ""),
-                        RowItem(arrayListOf("10", "07")),
-                        RowItem(arrayListOf("11", "07")),
-                        RowItem(arrayListOf("12", "07", "12", "24", "36", "48", "54"))
+                        RowItem(DayType.Weekday, arrayListOf("10", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("11", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("12", "07", "12", "24", "36", "48", "54"))
                     ), 3, 5),
 
                     // 2: Typical situation
                     arrayOf(Calendar.MONDAY, 11, 50, listOf(
                         HeaderItem(DayType.Weekday, ""),
-                        RowItem(arrayListOf("10", "07")),
-                        RowItem(arrayListOf("11", "07")),
-                        RowItem(arrayListOf("12", "07", "12", "24", "36", "48", "54"))
+                        RowItem(DayType.Weekday, arrayListOf("10", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("11", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("12", "07", "12", "24", "36", "48", "54"))
                     ), 3, 0),
 
                     // 3: Current hour is past last available one
                     arrayOf(Calendar.MONDAY, 13, 6, listOf(
                         HeaderItem(DayType.Weekday, ""),
-                        RowItem(arrayListOf("10", "07")),
-                        RowItem(arrayListOf("11", "07")),
-                        RowItem(arrayListOf("12", "07"))
+                        RowItem(DayType.Weekday, arrayListOf("10", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("11", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("12", "07"))
                     ), -1, -1),
 
                     // 4: It's sunday but the route only runs on weekdays
                     arrayOf(Calendar.SUNDAY, 13, 6, listOf(
                         HeaderItem(DayType.Weekday, ""),
-                        RowItem(arrayListOf("10", "07")),
-                        RowItem(arrayListOf("11", "07")),
-                        RowItem(arrayListOf("12", "07"))
+                        RowItem(DayType.Weekday, arrayListOf("10", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("11", "07")),
+                        RowItem(DayType.Weekday, arrayListOf("12", "07"))
                     ), -1, -1)
                 )
             }
@@ -121,6 +122,5 @@ class TimetableHelperTest {
         }
 
     }
-    */
 
 }
